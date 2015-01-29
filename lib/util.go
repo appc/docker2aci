b74dfd751c6eb3149f252a90b3bf55007871f823
Iago López Galeiras <iago@endocode.com>
2015-01-29 13:06:55 +0100

    Remove compression-handling stuff

--- lib/util.go
+++ lib/util.go
@@ -1,21 +1,8 @@
 package docker2aci
 
 import (
-	"bufio"
-	"bytes"
-	"compress/gzip"
-	"fmt"
-	"io"
-	"os"
 	"path"
 	"strings"
-)
-
-type Compression int
-
-const (
-	Uncompressed Compression = iota
-	Gzip
 )
 
 func makeEndpointsList(headers []string) []string {
@@ -32,38 +19,4 @@ func makeEndpointsList(headers []string) []string {
 	}
 
 	return endpoints
-}
-
-func decompress(layer io.Reader) (io.Reader, error) {
-	bufR := bufio.NewReader(layer)
-	bs, _ := bufR.Peek(10)
-
-	compression := detectCompression(bs)
-	switch compression {
-	case Gzip:
-		gz, err := gzip.NewReader(bufR)
-		if err != nil {
-			return nil, fmt.Errorf("Error reading layer (gzip): %v", err)
-		}
-		return gz, nil
-	case Uncompressed:
-		return bufR, nil
-	default:
-		return nil, fmt.Errorf("Unknown layer format")
-	}
-}
-
-func detectCompression(source []byte) Compression {
-	for compression, m := range map[Compression][]byte{
-		Gzip: {0x1F, 0x8B, 0x08},
-	} {
-		if len(source) < len(m) {
-			fmt.Fprintf(os.Stderr, "Len too short")
-			continue
-		}
-		if bytes.Compare(m, source[:len(m)]) == 0 {
-			return compression
-		}
-	}
-	return Uncompressed
 }
