appc/docker2aci

commit 47f2cb963409a14fb765eeb2c7e9ad0fa34ed448

backend/repository: pull layers in parallel
To reduce the time it takes to fetch docker images, this commit modifies
the backends to pull each image layer concurrently. As a result,
github.com/coreos/pkg/progressutil is used in place of ioprogress to
draw the progress bars.

lib/internal/backend/repository/repository2.go
@@ -34,7 +34,7 @@ import (
 	"github.com/appc/docker2aci/lib/internal/util"
 	"github.com/appc/docker2aci/pkg/log"
 	"github.com/appc/spec/schema"
-	"github.com/coreos/ioprogress"
+	"github.com/coreos/pkg/progressutil"
 )
 
 const (
@@ -66,42 +66,96 @@ func (rb *RepositoryBackend) getImageInfoV2(dockerURL *types.ParsedDockerURL) ([
 	return layers, dockerURL, nil
 }
 
-func (rb *RepositoryBackend) buildACIV2(layerNumber int, layerID string, dockerURL *types.ParsedDockerURL, outputDir string, tmpBaseDir string, curPwl []string, compression common.Compression) (string, *schema.ImageManifest, error) {
-	manifest := rb.imageManifests[*dockerURL]
+func (rb *RepositoryBackend) buildACIV2(layerIDs []string, dockerURL *types.ParsedDockerURL, outputDir string, tmpBaseDir string, compression common.Compression) ([]string, []*schema.ImageManifest, error) {
+	layerFiles := make([]*os.File, len(layerIDs))
+	layerDatas := make([]types.DockerImageData, len(layerIDs))
 
-	layerIndex, err := getLayerIndex(layerID, manifest)
+	tmpParentDir, err := ioutil.TempDir(tmpBaseDir, "docker2aci-")
 	if err != nil {
-		return "", nil, err
+		return nil, nil, err
 	}
+	defer os.RemoveAll(tmpParentDir)
 
-	if len(manifest.History) <= layerIndex {
-		return "", nil, fmt.Errorf("history not found for layer %s", layerID)
+	copier := &progressutil.CopyProgressPrinter{}
+
+	var errChannels []chan error
+	closers := make([]io.ReadCloser, len(layerIDs))
+	for i, layerID := range layerIDs {
+		errChan := make(chan error)
+		errChannels = append(errChannels, errChan)
+		// https://github.com/golang/go/wiki/CommonMistakes
+		i := i // golang--
+		layerID := layerID
+		go func() {
+			manifest := rb.imageManifests[*dockerURL]
+
+			layerIndex, err := getLayerIndex(layerID, manifest)
+			if err != nil {
+				errChan <- err
+				return
+			}
+
+			if len(manifest.History) <= layerIndex {
+				errChan <- fmt.Errorf("history not found for layer %s", layerID)
+				return
+			}
+
+			layerDatas[i] = types.DockerImageData{}
+			if err := json.Unmarshal([]byte(manifest.History[layerIndex].V1Compatibility), &layerDatas[i]); err != nil {
+				errChan <- fmt.Errorf("error unmarshaling layer data: %v", err)
+				return
+			}
+
+			tmpDir, err := ioutil.TempDir(tmpParentDir, "")
+			if err != nil {
+				errChan <- fmt.Errorf("error creating dir: %v", err)
+				return
+			}
+
+			layerFiles[i], closers[i], err = rb.getLayerV2(layerID, dockerURL, tmpDir, copier)
+			if err != nil {
+				errChan <- fmt.Errorf("error getting the remote layer: %v", err)
+				return
+			}
+			errChan <- nil
+		}()
 	}
-
-	layerData := types.DockerImageData{}
-	if err := json.Unmarshal([]byte(manifest.History[layerIndex].V1Compatibility), &layerData); err != nil {
-		return "", nil, fmt.Errorf("error unmarshaling layer data: %v", err)
-	}
-
-	tmpDir, err := ioutil.TempDir(tmpBaseDir, "docker2aci-")
+	err = copier.PrintAndWait(os.Stderr, 500*time.Millisecond, nil)
 	if err != nil {
-		return "", nil, fmt.Errorf("error creating dir: %v", err)
+		return nil, nil, err
 	}
-	defer os.RemoveAll(tmpDir)
-
-	layerFile, err := rb.getLayerV2(layerID, dockerURL, tmpDir)
-	if err != nil {
-		return "", nil, fmt.Errorf("error getting the remote layer: %v", err)
+	for _, closer := range closers {
+		closer.Close()
 	}
-	defer layerFile.Close()
+	for _, errChan := range errChannels {
+		err := <-errChan
+		if err != nil {
+			return nil, nil, err
+		}
+	}
+	for _, layerFile := range layerFiles {
+		err := layerFile.Sync()
+		if err != nil {
+			return nil, nil, err
+		}
+	}
+	var aciLayerPaths []string
+	var aciManifests []*schema.ImageManifest
+	var curPwl []string
+	for i := len(layerIDs) - 1; i >= 0; i-- {
+		log.Debug("Generating layer ACI...")
+		aciPath, aciManifest, err := internal.GenerateACI(i, layerDatas[i], dockerURL, outputDir, layerFiles[i], curPwl, compression)
+		if err != nil {
+			return nil, nil, fmt.Errorf("error generating ACI: %v", err)
+		}
+		aciLayerPaths = append(aciLayerPaths, aciPath)
+		aciManifests = append(aciManifests, aciManifest)
+		curPwl = aciManifest.PathWhitelist
 
-	log.Debug("Generating layer ACI...")
-	aciPath, aciManifest, err := internal.GenerateACI(layerNumber, layerData, dockerURL, outputDir, layerFile, curPwl, compression)
-	if err != nil {
-		return "", nil, fmt.Errorf("error generating ACI: %v", err)
+		layerFiles[i].Close()
 	}
 
-	return aciPath, aciManifest, nil
+	return aciLayerPaths, aciManifests, nil
 }
 
 func (rb *RepositoryBackend) getManifestV2(dockerURL *types.ParsedDockerURL) (*v2Manifest, []string, error) {
@@ -224,84 +278,61 @@ func getLayerIndex(layerID string, manifest v2Manifest) (int, error) {
 	return -1, fmt.Errorf("layer not found in manifest: %s", layerID)
 }
 
-func (rb *RepositoryBackend) getLayerV2(layerID string, dockerURL *types.ParsedDockerURL, tmpDir string) (*os.File, error) {
+func (rb *RepositoryBackend) getLayerV2(layerID string, dockerURL *types.ParsedDockerURL, tmpDir string, copier *progressutil.CopyProgressPrinter) (*os.File, io.ReadCloser, error) {
 	url := rb.schema + path.Join(dockerURL.IndexURL, "v2", dockerURL.ImageName, "blobs", layerID)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	rb.setBasicAuth(req)
 
 	res, err := rb.makeRequest(req, dockerURL.ImageName)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
-	defer res.Body.Close()
 
 	if res.StatusCode == http.StatusTemporaryRedirect || res.StatusCode == http.StatusFound {
 		location := res.Header.Get("Location")
 		if location != "" {
 			req, err = http.NewRequest("GET", location, nil)
 			if err != nil {
-				return nil, err
+				return nil, nil, err
 			}
 			res, err = rb.makeRequest(req, dockerURL.ImageName)
 			if err != nil {
-				return nil, err
+				return nil, nil, err
 			}
 			defer res.Body.Close()
 		}
 	}
 
 	if res.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("HTTP code: %d. URL: %s", res.StatusCode, req.URL)
+		return nil, nil, fmt.Errorf("HTTP code: %d. URL: %s", res.StatusCode, req.URL)
 	}
 
 	var in io.Reader
 	in = res.Body
 
-	if hdr := res.Header.Get("Content-Length"); hdr != "" {
-		imgSize, err := strconv.ParseInt(hdr, 10, 64)
-		if err != nil {
-			return nil, err
-		}
+	var size int64
 
-		prefix := "Downloading " + layerID[:18]
-		fmtBytesSize := 18
-		barSize := int64(80 - len(prefix) - fmtBytesSize)
-		bar := ioprogress.DrawTextFormatBarForW(barSize, os.Stderr)
-		fmtfunc := func(progress, total int64) string {
-			return fmt.Sprintf(
-				"%s: %s %s",
-				prefix,
-				bar(progress, total),
-				ioprogress.DrawTextFormatBytes(progress, total),
-			)
-		}
-		in = &ioprogress.Reader{
-			Reader:       res.Body,
-			Size:         imgSize,
-			DrawFunc:     ioprogress.DrawTerminalf(os.Stderr, fmtfunc),
-			DrawInterval: 500 * time.Millisecond,
+	if hdr := res.Header.Get("Content-Length"); hdr != "" {
+		size, err = strconv.ParseInt(hdr, 10, 64)
+		if err != nil {
+			return nil, nil, err
 		}
 	}
+
+	name := "Downloading " + layerID[:18]
 
 	layerFile, err := ioutil.TempFile(tmpDir, "dockerlayer-")
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
-	_, err = io.Copy(layerFile, in)
-	if err != nil {
-		return nil, err
-	}
+	copier.AddCopy(in, name, size, layerFile)
 
-	if err := layerFile.Sync(); err != nil {
-		return nil, err
-	}
-
-	return layerFile, nil
+	return layerFile, res.Body, nil
 }
 
 func (rb *RepositoryBackend) makeRequest(req *http.Request, repo string) (*http.Response, error) {

lib/docker2aci.go
@@ -124,31 +124,25 @@ func convertReal(backend internal.Docker2ACIBackend, dockerURL string, squash bo
 
 	conversionStore := newConversionStore()
 
+	// only compress individual layers if we're not squashing
+	layerCompression := compression
+	if squash {
+		layerCompression = common.NoCompression
+	}
+
+	aciLayerPaths, aciManifests, err := backend.BuildACI(ancestry, parsedDockerURL, layersOutputDir, tmpDir, layerCompression)
+	if err != nil {
+		return nil, err
+	}
+
 	var images acirenderer.Images
-	var aciLayerPaths []string
-	var curPwl []string
-	for i := len(ancestry) - 1; i >= 0; i-- {
-		layerID := ancestry[i]
-
-		// only compress individual layers if we're not squashing
-		layerCompression := compression
-		if squash {
-			layerCompression = common.NoCompression
-		}
-
-		aciPath, manifest, err := backend.BuildACI(i, layerID, parsedDockerURL, layersOutputDir, tmpDir, curPwl, layerCompression)
-		if err != nil {
-			return nil, fmt.Errorf("error building layer: %v", err)
-		}
-
-		key, err := conversionStore.WriteACI(aciPath)
+	for i, aciLayerPath := range aciLayerPaths {
+		key, err := conversionStore.WriteACI(aciLayerPath)
 		if err != nil {
 			return nil, fmt.Errorf("error inserting in the conversion store: %v", err)
 		}
 
-		images = append(images, acirenderer.Image{Im: manifest, Key: key, Level: uint16(i)})
-		aciLayerPaths = append(aciLayerPaths, aciPath)
-		curPwl = manifest.PathWhitelist
+		images = append(images, acirenderer.Image{Im: aciManifests[i], Key: key, Level: uint16(len(aciLayerPaths) - 1 - i)})
 	}
 
 	// acirenderer expects images in order from upper to base layer

lib/internal/backend/file/file.go
@@ -68,39 +68,48 @@ func (lb *FileBackend) GetImageInfo(dockerURL string) ([]string, *types.ParsedDo
 	return ancestry, parsedDockerURL, nil
 }
 
-func (lb *FileBackend) BuildACI(layerNumber int, layerID string, dockerURL *types.ParsedDockerURL, outputDir string, tmpBaseDir string, curPwl []string, compression common.Compression) (string, *schema.ImageManifest, error) {
-	tmpDir, err := ioutil.TempDir(tmpBaseDir, "docker2aci-")
-	if err != nil {
-		return "", nil, fmt.Errorf("error creating dir: %v", err)
+func (lb *FileBackend) BuildACI(layerIDs []string, dockerURL *types.ParsedDockerURL, outputDir string, tmpBaseDir string, compression common.Compression) ([]string, []*schema.ImageManifest, error) {
+	var aciLayerPaths []string
+	var aciManifests []*schema.ImageManifest
+	var curPwl []string
+	for i := len(layerIDs) - 1; i >= 0; i-- {
+		tmpDir, err := ioutil.TempDir(tmpBaseDir, "docker2aci-")
+		if err != nil {
+			return nil, nil, fmt.Errorf("error creating dir: %v", err)
+		}
+		defer os.RemoveAll(tmpDir)
+
+		j, err := getJson(lb.file, layerIDs[i])
+		if err != nil {
+			return nil, nil, fmt.Errorf("error getting image json: %v", err)
+		}
+
+		layerData := types.DockerImageData{}
+		if err := json.Unmarshal(j, &layerData); err != nil {
+			return nil, nil, fmt.Errorf("error unmarshaling layer data: %v", err)
+		}
+
+		tmpLayerPath := path.Join(tmpDir, layerIDs[i])
+		tmpLayerPath += ".tar"
+
+		layerFile, err := extractEmbeddedLayer(lb.file, layerIDs[i], tmpLayerPath)
+		if err != nil {
+			return nil, nil, fmt.Errorf("error getting layer from file: %v", err)
+		}
+		defer layerFile.Close()
+
+		log.Debug("Generating layer ACI...")
+		aciPath, manifest, err := internal.GenerateACI(i, layerData, dockerURL, outputDir, layerFile, curPwl, compression)
+		if err != nil {
+			return nil, nil, fmt.Errorf("error generating ACI: %v", err)
+		}
+
+		aciLayerPaths = append(aciLayerPaths, aciPath)
+		aciManifests = append(aciManifests, manifest)
+		curPwl = manifest.PathWhitelist
 	}
-	defer os.RemoveAll(tmpDir)
 
-	j, err := getJson(lb.file, layerID)
-	if err != nil {
-		return "", nil, fmt.Errorf("error getting image json: %v", err)
-	}
-
-	layerData := types.DockerImageData{}
-	if err := json.Unmarshal(j, &layerData); err != nil {
-		return "", nil, fmt.Errorf("error unmarshaling layer data: %v", err)
-	}
-
-	tmpLayerPath := path.Join(tmpDir, layerID)
-	tmpLayerPath += ".tar"
-
-	layerFile, err := extractEmbeddedLayer(lb.file, layerID, tmpLayerPath)
-	if err != nil {
-		return "", nil, fmt.Errorf("error getting layer from file: %v", err)
-	}
-	defer layerFile.Close()
-
-	log.Debug("Generating layer ACI...")
-	aciPath, manifest, err := internal.GenerateACI(layerNumber, layerData, dockerURL, outputDir, layerFile, curPwl, compression)
-	if err != nil {
-		return "", nil, fmt.Errorf("error generating ACI: %v", err)
-	}
-
-	return aciPath, manifest, nil
+	return aciLayerPaths, aciManifests, nil
 }
 
 func getImageID(file *os.File, dockerURL *types.ParsedDockerURL) (string, *types.ParsedDockerURL, error) {

lib/internal/backend/repository/repository.go
@@ -93,11 +93,11 @@ func (rb *RepositoryBackend) GetImageInfo(url string) ([]string, *types.ParsedDo
 	}
 }
 
-func (rb *RepositoryBackend) BuildACI(layerNumber int, layerID string, dockerURL *types.ParsedDockerURL, outputDir string, tmpBaseDir string, curPwl []string, compression common.Compression) (string, *schema.ImageManifest, error) {
+func (rb *RepositoryBackend) BuildACI(layerIDs []string, dockerURL *types.ParsedDockerURL, outputDir string, tmpBaseDir string, compression common.Compression) ([]string, []*schema.ImageManifest, error) {
 	if rb.hostsV2Support[dockerURL.IndexURL] {
-		return rb.buildACIV2(layerNumber, layerID, dockerURL, outputDir, tmpBaseDir, curPwl, compression)
+		return rb.buildACIV2(layerIDs, dockerURL, outputDir, tmpBaseDir, compression)
 	} else {
-		return rb.buildACIV1(layerNumber, layerID, dockerURL, outputDir, tmpBaseDir, curPwl, compression)
+		return rb.buildACIV1(layerIDs, dockerURL, outputDir, tmpBaseDir, compression)
 	}
 }
 

lib/internal/backend/repository/repository1.go
@@ -62,36 +62,73 @@ func (rb *RepositoryBackend) getImageInfoV1(dockerURL *types.ParsedDockerURL) ([
 	return ancestry, dockerURL, nil
 }
 
-func (rb *RepositoryBackend) buildACIV1(layerNumber int, layerID string, dockerURL *types.ParsedDockerURL, outputDir string, tmpBaseDir string, curPwl []string, compression common.Compression) (string, *schema.ImageManifest, error) {
-	tmpDir, err := ioutil.TempDir(tmpBaseDir, "docker2aci-")
+func (rb *RepositoryBackend) buildACIV1(layerIDs []string, dockerURL *types.ParsedDockerURL, outputDir string, tmpBaseDir string, compression common.Compression) ([]string, []*schema.ImageManifest, error) {
+	layerFiles := make([]*os.File, len(layerIDs))
+	layerDatas := make([]types.DockerImageData, len(layerIDs))
+
+	tmpParentDir, err := ioutil.TempDir(tmpBaseDir, "docker2aci-")
 	if err != nil {
-		return "", nil, fmt.Errorf("error creating dir: %v", err)
+		return nil, nil, err
 	}
-	defer os.RemoveAll(tmpDir)
+	defer os.RemoveAll(tmpParentDir)
 
-	j, size, err := rb.getJsonV1(layerID, rb.repoData.Endpoints[0], rb.repoData)
-	if err != nil {
-		return "", nil, fmt.Errorf("error getting image json: %v", err)
+	var doneChannels []chan error
+	for i, layerID := range layerIDs {
+		doneChan := make(chan error)
+		doneChannels = append(doneChannels, doneChan)
+		// https://github.com/golang/go/wiki/CommonMistakes
+		i := i // golang--
+		layerID := layerID
+		go func() {
+			tmpDir, err := ioutil.TempDir(tmpParentDir, "")
+			if err != nil {
+				doneChan <- fmt.Errorf("error creating dir: %v", err)
+				return
+			}
+
+			j, size, err := rb.getJsonV1(layerID, rb.repoData.Endpoints[0], rb.repoData)
+			if err != nil {
+				doneChan <- fmt.Errorf("error getting image json: %v", err)
+				return
+			}
+
+			layerDatas[i] = types.DockerImageData{}
+			if err := json.Unmarshal(j, &layerDatas[i]); err != nil {
+				doneChan <- fmt.Errorf("error unmarshaling layer data: %v", err)
+				return
+			}
+
+			layerFiles[i], err = rb.getLayerV1(layerID, rb.repoData.Endpoints[0], rb.repoData, size, tmpDir)
+			if err != nil {
+				doneChan <- fmt.Errorf("error getting the remote layer: %v", err)
+				return
+			}
+		}()
+	}
+	for _, doneChan := range doneChannels {
+		err := <-doneChan
+		if err != nil {
+			return nil, nil, err
+		}
+	}
+	var aciLayerPaths []string
+	var aciManifests []*schema.ImageManifest
+	var curPwl []string
+
+	for i := len(layerIDs) - 1; i >= 0; i-- {
+		log.Debug("Generating layer ACI...")
+		aciPath, manifest, err := internal.GenerateACI(i, layerDatas[i], dockerURL, outputDir, layerFiles[i], curPwl, compression)
+		if err != nil {
+			return nil, nil, fmt.Errorf("error generating ACI: %v", err)
+		}
+		aciLayerPaths = append(aciLayerPaths, aciPath)
+		aciManifests = append(aciManifests, manifest)
+		curPwl = manifest.PathWhitelist
+
+		layerFiles[i].Close()
 	}
 
-	layerData := types.DockerImageData{}
-	if err := json.Unmarshal(j, &layerData); err != nil {
-		return "", nil, fmt.Errorf("error unmarshaling layer data: %v", err)
-	}
-
-	layerFile, err := rb.getLayerV1(layerID, rb.repoData.Endpoints[0], rb.repoData, size, tmpDir)
-	if err != nil {
-		return "", nil, fmt.Errorf("error getting the remote layer: %v", err)
-	}
-	defer layerFile.Close()
-
-	log.Debug("Generating layer ACI...")
-	aciPath, manifest, err := internal.GenerateACI(layerNumber, layerData, dockerURL, outputDir, layerFile, curPwl, compression)
-	if err != nil {
-		return "", nil, fmt.Errorf("error generating ACI: %v", err)
-	}
-
-	return aciPath, manifest, nil
+	return aciLayerPaths, aciManifests, nil
 }
 
 func (rb *RepositoryBackend) getRepoDataV1(indexURL string, remote string) (*RepoData, error) {

lib/internal/internal.go
@@ -52,7 +52,7 @@ import (
 // path and its converted ImageManifest.
 type Docker2ACIBackend interface {
 	GetImageInfo(dockerUrl string) ([]string, *types.ParsedDockerURL, error)
-	BuildACI(layerNumber int, layerID string, dockerURL *types.ParsedDockerURL, outputDir string, tmpBaseDir string, curPWl []string, compression common.Compression) (string, *schema.ImageManifest, error)
+	BuildACI(layerIDs []string, dockerURL *types.ParsedDockerURL, outputDir string, tmpBaseDir string, compression common.Compression) ([]string, []*schema.ImageManifest, error)
 }
 
 // GenerateACI takes a Docker layer and generates an ACI from it.

commit 08bd6909cfac1c15df145e4b9a125ade3a33e4ca

lib/repository2: fix parallel pull synchronisation
The parallel pull refactoring in
47f2cb963409a14fb765eeb2c7e9ad0fa34ed448 introduced an unfortunate and
subtle race condition. Because i) we were asynchronously starting the
fetching of each layer [1][1] (which implies that the fetch is added to
the progress bar asynchronously, via getLayerV2 [2][2] [3][3]), and ii)
the progressutil package has no guards against calling PrintAndWait and
AddCopy in indeterminate order [4][4], it was possible for us to enter
the PrintAndWait loop [5][5] [6][6] before all of the layer fetches had
actually been added to it. Then, in the event that the first layer was
particularly fast, the CopyProgressPrinter could actually think it was
done [7][7] before the other layer(s) had finished. In this situation,
docker2aci would happily continue forward and try to generate an ACI
from each layer [8][8], and for any layer(s) that had not actually
finished downloading, the GenerateACI->writeACI->tarball.Walk call
[9][9] would be operating on a partially written file and hence result
in the errors we've been seeing ("unexpected EOF", "corrupt input
before offset 45274", and so forth).

A great case to reproduce this is the `docker:busybox` image because of
its particular characteristics: two layers, one very small (32B) and one
relatively larger (676KB) layer. Typical output looks like the following

```
% $(exit 0); while [ $? -eq 0 ]; do ./bin/docker2aci docker://busybox; done

Downloading sha256:385e281300c [===============================] 676 KB
/ 676 KB
Downloading sha256:a3ed95caeb0 [===============================]     32
B / 32 B

Generated ACI(s):
library-busybox-latest.aci

Downloading sha256:a3ed95caeb0 [==============================]      32
B / 32 B
Downloading sha256:385e281300c [==============================]  676 KB
/ 676 KB

Generated ACI(s):
library-busybox-latest.aci

Downloading sha256:a3ed95caeb0 [===================================] 32
B / 32 B

lib/internal/backend/repository/repository2.go
@@ -26,6 +26,7 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/appc/docker2aci/lib/common"
@@ -80,13 +81,17 @@ func (rb *RepositoryBackend) buildACIV2(layerIDs []string, dockerURL *types.Pars
 
 	var errChannels []chan error
 	closers := make([]io.ReadCloser, len(layerIDs))
+	var wg sync.WaitGroup
 	for i, layerID := range layerIDs {
-		errChan := make(chan error)
+		wg.Add(1)
+		errChan := make(chan error, 1)
 		errChannels = append(errChannels, errChan)
 		// https://github.com/golang/go/wiki/CommonMistakes
 		i := i // golang--
 		layerID := layerID
 		go func() {
+			defer wg.Done()
+
 			manifest := rb.imageManifests[*dockerURL]
 
 			layerIndex, err := getLayerIndex(layerID, manifest)
@@ -120,6 +125,8 @@ func (rb *RepositoryBackend) buildACIV2(layerIDs []string, dockerURL *types.Pars
 			errChan <- nil
 		}()
 	}
+	// Need to wait for all of the readers to be added to the copier (which happens during rb.getLayerV2)
+	wg.Wait()
 	err = copier.PrintAndWait(os.Stderr, 500*time.Millisecond, nil)
 	if err != nil {
 		return nil, nil, err