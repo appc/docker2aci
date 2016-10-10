85eb914dde8252110af29b704fdf235b34475234
Luca Bruno <lucab@debian.org>
2016-10-10 14:05:02 +0000

    tests: fixtures and checks for invalid layer IDs

---
+++ tests/fixture-test-invalidlayerid/check.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+DOCKER2ACI=../bin/docker2aci
+TESTDIR=$1
+TESTNAME=$2
+
+sudo ${DOCKER2ACI} "${TESTDIR}/${TESTNAME}/${TESTNAME}.docker"
+if [ $? -eq 1 ]; then
+	echo "### Test case ${TESTNAME}: SUCCESS"
+	exit 0
+else
+	echo "### Test case ${TESTNAME}: FAIL"
+	exit 1
+fi
+
