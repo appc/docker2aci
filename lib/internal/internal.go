28e6553343cb6c9cb44275a2aec87f4d38279d66
Greg Poirier <grep@stripe.com>
2016-10-19 16:04:52 -0700

    Map Docker labels to appc annotations.

--- lib/internal/internal.go
+++ lib/internal/internal.go
@@ -271,6 +271,10 @@ func GenerateManifest(layerData types.DockerImageData, dockerURL *types.ParsedDo
 
 			genManifest.App = app
 		}
+
+		for k, v := range dockerConfig.Labels {
+			addAnno(k, v)
+		}
 	}
 
 	if layerData.Parent != "" {
